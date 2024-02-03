24c5d47b7408a3b51a14cb858d9b2de40ac4f809
falakjaiin <156042591+falakjaiin@users.noreply.github.com>
2024-02-02 19:44:31 -0800

    Update scriptcalculator.js

--- scriptcalculator.js
+++ scriptcalculator.js
@@ -1,4 +1,5 @@
 
+var expr = '';
 document.addEventListener('DOMContentLoaded',function()
     {
         exp_input = document.getElementById('Expression');
@@ -7,13 +8,13 @@ document.addEventListener('DOMContentLoaded',function()
 
         buttons.forEach( function(button)
         {
-            button.addEventListener('click', function()
-            {
+            button.addEventListener('click', function() {
                 input_button(button.textContent);
             });
         });
 
 
+        
         function input_button(expr)
         {
             switch(expr)
@@ -79,5 +80,56 @@ document.addEventListener('DOMContentLoaded',function()
 
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    expressionInput = document.getElementById('expression');
+ 
+    buttons = document.querySelectorAll('button');
+ 
+   buttons.forEach(function (button) {
+     button.addEventListener('click', function () {
+       handleButtonClick(button.textContent);
+     });
+   });
+ 
+   function handleButtonClick(value) {
+     switch (value) {
+       case '=':
+         calculateResult();
+         break;
+       case 'Delete':
+         deleteLastCharacter();
+         break;
+       case 'clear':
+         clearExpression();
+         break;
+        default:
+         appendToExpression(value);
+         break;
+     }
+   }
+ 
+   function appendToExpression(value) {
+     expressionInput.value += value;
+   }
+ 
+   function deleteLastCharacter() {
+     expressionInput.value = expressionInput.value.slice(0, -1);
+   }
+ 
+   function clearExpression() {
+     expressionInput.value = '';
+   }
+ 
+ 
+   function calculateResult() {
+     try {
+       const result = eval(expressionInput.value);
+       expressionInput.value = result;
+     } catch (error) {
+       expressionInput.value = 'Error';
+     }
+   }
+ });
+ 
 
     
