65f17da72ba1d4b3a20cd334f4d8019834ebcda7
falakjaiin <156042591+falakjaiin@users.noreply.github.com>
2024-02-12 13:57:52 -0800

    Create lab6jscode.js

---
+++ lab6jscode.js
@@ -0,0 +1,88 @@
+
+    document.addEventListener('DOMContentLoaded', () => 
+    {
+        const messages = document.querySelector('#messages');
+        messages.style.display = 'none';
+        const submitButton = document.querySelector('#submitButton');
+        submitButton.addEventListener('click', submitForm);
+    });
+    async  function submitForm() 
+    {
+        const name = document.querySelector('#name').value.trim();
+        const feedback = document.querySelector('#feedback').value.trim();
+        if (name || feedback) 
+        {
+            const data = 
+            {
+                name: name,
+                feedback: feedback
+            };
+            try 
+            {
+                const response = await fetch('https://fun165-98f8a-default-rtdb.firebaseio.com/chatMsg.json', 
+                {
+                    method: 'POST',
+                    headers: 
+                    {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                });
+
+                if (!response.ok) 
+                {
+                    throw new Error('Error saving data');
+                }
+                //hideForm();
+                hideFormFetchDataWithAsyncAwait();
+            }
+            catch (error) 
+            {
+                console.error('Error saving data: ', error.message);
+            }
+        } 
+        else 
+        {
+            alert('Please fill in the field of course feedback.');
+        }
+
+    }
+    function hideForm() 
+    {
+        const submitForm = document.querySelector('#submitForm');
+        const messages = document.querySelector('#messages');
+
+        submitForm.style.display = 'none';
+        messages.style.display = 'block';
+    }
+    async function hideFormFetchDataWithAsyncAwait() 
+    {
+        hideForm();
+        try
+        {
+            const response = await fetch('https://fun165-98f8a-default-rtdb.firebaseio.com/chatMsg.json');
+            if (!response.ok)
+            {
+                throw new Error('Error in fetching data.');
+            }
+            const data = await response.json();
+            displayMessages(data);
+        }
+        catch(error)
+        {
+            console.error('Error fetching data: ', error.message);
+        }
+    }
+
+    function displayMessages(data)
+    {
+        const messagesContainer = document.querySelector('#messages');
+        messagesContainer.innerHTML = '';
+        for (const postId in data) 
+        {
+            const post = data[postId];
+            const post_element = document.createElement('div');
+            post_element.textContent = `${post.name}: ${post.feedback}`;
+            messagesContainer.appendChild(post_element);
+        }
+    }
